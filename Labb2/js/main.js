ef21d4c86203ab37f2bd23d53851bdfa313a37fc
Oscar <osfr1701@student.miun.se>
2019-01-25 13:08:10 +0100

    babysteps

--- Labb2/js/main.js
+++ Labb2/js/main.js
@@ -1,40 +1,65 @@
-"use strict";
+"use strict";   //Kör i strikt läge. Ger t.ex error om variabler som inte är deklarerade används
 
 var baseURL = "http://api.arbetsformedlingen.se/af/v0";
 
-//
-// Wait for DOM to load
-document.addEventListener("DOMContentLoaded", function(){ 
+
+//Körs när hemsidan öppnas??
+document.addEventListener("DOMContentLoaded", function()
+{ 
     var xmlhttp = new XMLHttpRequest();
 
-//
-// Read all LÃ„N and dynamically create list from AF
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-           if (xmlhttp.status == 200) {
-
-                var jsonData = JSON.parse( xmlhttp.responseText );
+    // Read all LÄN and dynamically create list from AF
+    xmlhttp.onreadystatechange = function() 
+    {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) 
+        {
+           if (xmlhttp.status == 200) 
+           {
                 document.getElementById("searchlan").innerHTML = "";
-                for(var i=0; i < jsonData.soklista.sokdata.length; i++){
-                   document.getElementById("mainnavlist").innerHTML += "<li id='"+jsonData.soklista.sokdata[i].id+"'>"+jsonData.soklista.sokdata[i].namn+" (" + jsonData.soklista.sokdata[i].antal_ledigajobb + ")</li>";    
-                   document.getElementById("searchlan").innerHTML += "<option value='"+jsonData.soklista.sokdata[i].id+"'>"+jsonData.soklista.sokdata[i].namn+"</option>";      
+                var jsonData = JSON.parse(xmlhttp.responseText);
+                for(var i = 0; i < jsonData.soklista.sokdata.length; i++)
+                {
+                   document.getElementById("mainnavlist").innerHTML += "<li id='" + jsonData.soklista.sokdata[i].id + "'>" + jsonData.soklista.sokdata[i].namn + " (" + jsonData.soklista.sokdata[i].antal_ledigajobb + ")</li>";    
+                   document.getElementById("searchlan").innerHTML += "<option value='" + jsonData.soklista.sokdata[i].id + "'>" + jsonData.soklista.sokdata[i].namn + "</option>";      
                 }
            }
-           else if (xmlhttp.status == 400) {
-              alert('There was an error 400');
-           }
-           else {
-               alert('something else other than 200 was returned');
-           }
+           else if (xmlhttp.status == 400)  alert('There was an error 400');
+           else                             alert('something else other than 200 was returned');
+           
         }
     };
 
-    xmlhttp.open("GET", baseURL+"/platsannonser/soklista/lan", true);
+    xmlhttp.open("GET", baseURL + "/platsannonser/soklista/lan", true);
     xmlhttp.send();
 }); 
 
-//
-// Create eventlistener for clicks on dynamically created list of LÃ„N in mainnavlist
-document.getElementById('mainnavlist').addEventListener("click", function(e){
-    alert("Du har valt lÃ¤n med id "+e.target.id);
-}); 
+
+//Körs när ett av länen i listan klickas
+document.getElementById('mainnavlist').addEventListener("click", function(e)
+{    
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() 
+    {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) 
+        {
+           if (xmlhttp.status == 200) 
+           {
+                //alert(xmlhttp.responseText);
+
+                var jsonData = JSON.parse(xmlhttp.responseText);
+                
+                //Hantera hämtad data
+                
+           }
+           else if (xmlhttp.status == 400)  alert('There was an error 400');
+           else                             alert('something else other than 200 was returned');
+           
+        }
+    };
+
+    var request = baseURL + "/platsannonser/matchning?lanid=" + e.target.id;
+    xmlhttp.open("GET", request, true);
+    xmlhttp.send();
+
+});
