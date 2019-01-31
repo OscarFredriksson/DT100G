5453244b6c616ce6cdca7593545fefed9c30344c
Oscar <osfr1701@student.miun.se>
2019-01-31 11:52:25 +0100

    sök funkar

--- Labb2/js/main.js
+++ Labb2/js/main.js
@@ -3,13 +3,16 @@
 var baseURL = "http://api.arbetsformedlingen.se/af/v0";
 
 //Körs när hemsidan öppnas
-document.addEventListener("DOMContentLoaded", loadFront(), false); 
+document.addEventListener("DOMContentLoaded", loadFront, false); 
 document.getElementById("logo").addEventListener('click', loadFront, false);
 
 function loadFront()
 {
     var lanlista = document.getElementById("mainnavlist");
     lanlista.innerHTML = "";
+
+    var lanDropDown = document.getElementById("searchlan");
+    lanDropDown.innerHTML = "";
 
     document.getElementById("info").innerHTML = "";
 
@@ -24,11 +27,12 @@ function loadFront()
            {
                 document.getElementById("searchlan").innerHTML = "";
                 var jsonData = JSON.parse(xmlhttp.responseText);
-                for(var i = 0; i < jsonData.soklista.sokdata.length; i++)
+
+                jsonData.soklista.sokdata.forEach(function(data)
                 {
-                    lanlista.innerHTML += "<li id='" + jsonData.soklista.sokdata[i].id + "'>" + jsonData.soklista.sokdata[i].namn + " (" + jsonData.soklista.sokdata[i].antal_ledigajobb + ")</li>";    
-                    document.getElementById("searchlan").innerHTML += "<option value='" + jsonData.soklista.sokdata[i].id + "'>" + jsonData.soklista.sokdata[i].namn + "</option>";      
-                }
+                    lanlista.innerHTML += "<li id='" + data.id + "'>" + data.namn + " (" + data.antal_ledigajobb + ")</li>";    
+                    lanDropDown.innerHTML += "<option value='" + data.id + "'>" + data.namn + "</option>";      
+                });
            }
            else if (xmlhttp.status == 400)  alert('There was an error 400');
            else                             alert('something else other than 200 was returned');
@@ -40,10 +44,28 @@ function loadFront()
     xmlhttp.send();
 };
 
+function loadData(data)
+{
+    var element = document.getElementById("info");
+                    
+    //Data:
+    element.innerHTML += "<h3>" + data.annonsrubrik + "</h3>";
+    element.innerHTML += "<h4>" + data.yrkesbenamning + "</h4>";
+    element.innerHTML += "<h4>" + "Anställningstyp: " +  "??????????" + "</h4>";
+    element.innerHTML += "<h4>" + "Antal platser: " + data.antalplatser + "</h4>";
+    element.innerHTML += "<h4>" + "Publicersingsdatum: " + data.publiceraddatum + "</h4>";
+    element.innerHTML += "<h4>" + "Sista ansökningsdag: " + "??????????" + "</h4>";
+    
+    //Knapp:
+    element.innerHTML += "<form action='" + data.annonsurl + "'><button type='searchbutton' class='btn'> Läs Mer </button></form>";
+    
+    element.innerHTML += "<hr>";    //Divider line
+}
 
-//Körs när ett av länen i listan klickas
-document.getElementById('mainnavlist').addEventListener("click", function(e)
-{    
+function loadRegionContent(regionID)
+{
+    document.getElementById("info").innerHTML = "";
+
     var xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = function() 
@@ -54,24 +76,7 @@ document.getElementById('mainnavlist').addEventListener("click", function(e)
            {
                 var jsonData = JSON.parse(xmlhttp.responseText);
                 
-                //Hantera hämtad data
-                for(var i = 0; i < jsonData.matchningslista.matchningdata.length; i++)
-                {
-                    var element = document.getElementById("info");
-
-                    //Data:
-                    element.innerHTML += "<h3>" + jsonData.matchningslista.matchningdata[i].annonsrubrik + "</h3>";
-                    element.innerHTML += "<h4>" + jsonData.matchningslista.matchningdata[i].yrkesbenamning + "</h4>";
-                    element.innerHTML += "<h4>" + "Anställningstyp: " +  "??????????" + "</h4>";
-                    element.innerHTML += "<h4>" + "Antal platser: " + jsonData.matchningslista.matchningdata[i].antalplatser + "</h4>";
-                    element.innerHTML += "<h4>" + "Publicersingsdatum: " + jsonData.matchningslista.matchningdata[i].publiceraddatum + "</h4>";
-                    element.innerHTML += "<h4>" + "Sista ansökningsdag: " + "??????????" + "</h4>";
-                    
-                    //Knapp:
-                    element.innerHTML += "<form action='" + jsonData.matchningslista.matchningdata[i].annonsurl + "'><button type='searchbutton' class='btn'> Läs Mer </button></form>";
-                    
-                    element.innerHTML += "<hr>";    //Divider line
-                }
+                jsonData.matchningslista.matchningdata.forEach(loadData);   //Ladda in data              
            }
            else if (xmlhttp.status == 400)  alert('There was an error 400');
            else                             alert('something else other than 200 was returned');
@@ -79,8 +84,54 @@ document.getElementById('mainnavlist').addEventListener("click", function(e)
         }
     };
 
-    var request = baseURL + "/platsannonser/matchning?lanid=" + e.target.id;
+    var request = baseURL + "/platsannonser/matchning?lanid=" + regionID
     xmlhttp.open("GET", request, true);
     xmlhttp.send();
 
+}
+
+//Körs när ett av länen i listan klickas
+document.getElementById('mainnavlist').addEventListener("click", function(e)
+{    
+    loadRegionContent(e.target.id);
+    document.getElementById("searchlan").value = e.target.id;
+});
+
+document.getElementById("searchbutton").addEventListener("click", function()
+{
+    var searchText = document.getElementById("searchText").value;     
+
+    if(!searchText) return;
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() 
+    {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) 
+        {
+           if (xmlhttp.status == 200) 
+           {
+                var jsonData = JSON.parse(xmlhttp.responseText);
+                
+                var element = document.getElementById("info");
+                element.innerHTML = "";
+                
+                jsonData.soklista.sokdata.forEach(function(data)
+                {
+                    element.innerHTML += "<h3>" + data.namn + "</h3>";
+                    element.innerHTML += "<h4>" + "Antal platsannonser: " + data.antal_platsannonser + "</h4>";
+                    element.innerHTML += "<h4>" + "Antal lediga jobb:" + data.antal_ledigajobb + "</h4>";
+                    
+                    element.innerHTML += "<hr>";    //Divider line
+                });                
+           }
+           else if (xmlhttp.status == 400)  alert('There was an error 400');
+           else                             alert('something else other than 200 was returned');
+           
+        }
+    };
+
+    var request = baseURL + "/platsannonser/soklista/yrken/" + searchText;
+    xmlhttp.open("GET", request, true);
+    xmlhttp.send();
 });
