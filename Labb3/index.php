bee65955ab3f25cdc1efff1f691e678f40848dd0
Oscar Fredriksson <osfr1701@student.miun.se>
2019-02-22 02:41:16 +0100

    småfix

--- Labb3/index.php
+++ Labb3/index.php
@@ -7,39 +7,39 @@
             <?php $currentPage = 'index' ?>
             <?php include("includes/header.php"); ?>
 
-            <ul class="flat-box">
-                <li>
+            <ul class="box-list">
+                <li class="flat-box">
                     <p class="upper-text"> Har du tidigare erfarenhet av utveckling med PHP? </p>
                     <hr>
                     <p class="lower-text"> Nej </p>
                 </li>
                 
-                <li> 
+                <li class="flat-box"> 
                     <p class="upper-text"> Hur har du valt att strukturera upp dina filer och kataloger? </p>
                     <hr>
                     <p class="lower-text"> Jag har lagt alla php filer som includeras inom andra filer i en egen mapp, resten ligger i grundkatalogen. </p> 
                 </li> 
                 
                 
-                <li> 
+                <li class="flat-box"> 
                     <p class="upper-text"> Har du följt guiden, eller skapat på egen hand? </p>
                     <hr>
                     <p class="lower-text"> Tog inspiration från guiden för include-mappen, men skippade css mappen då den inte kändes nödvändig när jag endast har en css-fil. </p>
                 </li>
                 
-                <li> 
+                <li class="flat-box"> 
                     <p class="upper-text"> Har du gjort några förbättringar eller vidareutvecklingar av guiden (om du följt denna)?</p>
                     <hr>
                     <p class="lower-text"> ..... </p> 
                 </li>
                 
-                <li> 
+                <li class="flat-box"> 
                     <p class="upper-text"> Vilken utvecklingsmiljö har du använt för uppgiften (Editor, webbserver etcetera)? </p>
                     <hr>
                     <p class="lower-text"> Visual Studio Code + XAMPP </p>
                 </li>
                 
-                <li> 
+                <li class="flat-box"> 
                     <p class="upper-text"> Har något varit svårt med denna uppgift? </p>
                     <hr>
                     <p class="lower-text"> Php var klurigt i början, men när man började förstå syftet med det så flöt det mesta på. </p> 
--- Labb3/info.php
+++ Labb3/info.php
@@ -7,26 +7,26 @@
             <?php $currentPage = 'info' ?>
             <?php include("includes/header.php"); ?>
 
-            <ul class="flat-box">
-                <li> 
+            <ul class="box-list">
+                <li class="flat-box">
                     <p class="upper-text"> Datum/Klockslag </p>
                     <hr>
                     <p class="lower-text"> <?php echo date("Y-m-d H:i:s") ?> </p> 
                 </li> 
                 
-                <li> 
+                <li class="flat-box"> 
                     <p class="upper-text"> IP-adress </p>
                     <hr>
                     <p class="lower-text"> <?php echo $_SERVER['REMOTE_ADDR'] ?> </p> 
                 </li>
                 
-                <li> 
+                <li class="flat-box"> 
                     <p class="upper-text"> Sökväg/Filnamn </p>
                     <hr>
                     <p class="lower-text"> <?php echo getcwd() ?> </p>
                 </li>
                 
-                <li> 
+                <li class="flat-box"> 
                     <p class="upper-text"> User agent-sträng </p>
                     <hr>
                     <p class="lower-text"> <?php echo $_SERVER['HTTP_USER_AGENT'] ?> </p> 
