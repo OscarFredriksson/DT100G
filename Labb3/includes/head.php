ef13a3e300e3f19540549665a2b2586dc8520991
Oscar Fredriksson <osfr1701@student.miun.se>
2019-02-22 15:33:43 +0100

    skapade en css mapp

--- Labb3/includes/head.php
+++ Labb3/includes/head.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="style.css"> <!--Importera stil-mallen-->
+    <link rel="stylesheet" type="text/css" href="css/style.css"> <!--Importera stil-mallen-->
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     
