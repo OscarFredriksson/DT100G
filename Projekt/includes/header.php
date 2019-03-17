89c4683407141fa420d0a4a8a9f7e33a1a1e8d14
Oscar Fredriksson <osfr1701@student.miun.se>
2019-03-17 23:28:47 +0100

    Påbörjade projekt

---
+++ Projekt/includes/header.php
@@ -0,0 +1,5 @@
+<header>
+    
+    <p class="header-title"> Projekt - Quiz </p>
+
+</header>
---
+++ Projekt/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Sniglet|Open+Sans">
+        
+        <title> Quiz </title>
+    </head>
+
+    <body>
+        <?php include "includes/header.php"; ?>
+
+        <div class="content">
+            <div class="question">
+                <h1> Frågan här... </h1>
+            </div>
+
+            <div class="alternatives">
+                <input class="alternative" value="Alternativ 1">
+
+                <input class="alternative" value="Alternativ 2">
+                
+                <input class="alternative" value="Alternativ 3">
+                
+                <input class="alternative" value="Alternativ 4">
+
+            </div>
+        <div>
+
+        <?php include "includes/footer.php"; ?>
+    </body>
+
+    <script src="js/main.js"></script>
+</html>
